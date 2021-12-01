9c83cada88b7906054510941474bb4de5d74d041
rj <rjahrj@gmail.com>
2021-12-01 02:31:18 +0000

    finished rewrite

--- aa.sh
+++ aa.sh
@@ -9,8 +9,7 @@ setup_terminal() {
   # '\e[2J':     Clear the screen.
   # '\e[1;Nr':   Limit scrolling to scrolling area.
   #              Also sets cursor to (0,0).
-  printf '\e[?1049h' "$max_items"
-  # \e[?7l\e[?25l\e[2J\e[1;%sr' "$max_items"
+  printf '\e[?1049h\e[?7l\e[?25l\e[2J\e[1;%sr' "$max_items"
 
     # Hide echoing of user input
     # stty -echo
@@ -25,12 +24,33 @@ get_term_size() {
 set_alias_file() {
   # Set default alias file
   default_file=~/.bash_aliases
-
+  # Set alias from config file
   my_alias_file=${my_file:-$default_file}
+}
 
-  echo $my_alias_file
+add_alias() {
+  # Get the name of the alias
+  read -p 'What is the alias name: ' new_alias_name
+  echo $new_alias_name
+  # Get alias command
+  read -p "What will $new_alias_name do? " new_alias_command
+  echo $new_alias_command
+  save_alias
+}
 
+save_alias() {
 
+  echo Would you like to save $new_alias_name [yY/nN]
+  while true; do
+    read -p "$1" 'yn'
+    if [ "$yn" = "" ]; then
+      yn='Y'
+    fi
+    case $yn in
+      [yY] ) echo "alias $new_alias_name=\"$new_alias_command\"" >> $my_alias_file; break;;
+      [nN] ) echo "Aborting..."; exit;;
+    esac
+  done
 
 }
 
@@ -39,6 +59,7 @@ main() {
   get_term_size
   setup_terminal
   set_alias_file
+  add_alias
 
 }
 
